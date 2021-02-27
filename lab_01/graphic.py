d1e80863f6a2c985a4526b967703fba631ce4ea7
mmd19u540 <mymidi.student@yandex.ru>
2021-02-27 22:59:25 +0300

    ADD: printing point info

--- lab_01/graphic.py
+++ lab_01/graphic.py
@@ -41,9 +41,10 @@ def create_point(canvas, point):
         Функция отрисовки точки
     """
     # print(point)
+    size = canvas.winfo_height() / 150
 
-    x1, y1 = (point[0] - 5), (point[1] - 5)
-    x2, y2 = (point[0] + 5), (point[1] + 5)
+    x1, y1 = (point[0] - size), (point[1] - size)
+    x2, y2 = (point[0] + size), (point[1] + size)
 
     canvas.create_oval(x1, y1, x2, y2, fill="#476042")
 
@@ -56,6 +57,24 @@ def create_triangle(canvas, vertexes):
     canvas.create_polygon(*vertexes, outline="#000000", fill="#ffffff", width=4)
 
 
+def point_place(base_point, point):
+    """
+        Определение положения точки point
+        относительно base_point
+    """
+
+    if (point[0] < base_point[0]):
+        if (point[1] > base_point[1]):
+            return 0
+
+        return 3
+
+    if (point[1] > base_point[1]):
+        return 1
+
+    return 2
+
+
 def print_point_info(canvas, nums, canvas_points, true_points):
     """
         Вывод номеров и координат точек
@@ -63,4 +82,38 @@ def print_point_info(canvas, nums, canvas_points, true_points):
 
     for i, point in enumerate(true_points):
         info = "{:3d}({:6.2f}, {:6.2f})".format(nums[i] + 1, point[0], point[1])
-        canvas.create_text(canvas_points[i], text=info)
+
+        x_place = canvas_points[i][0] 
+        y_place = canvas_points[i][1]
+
+        point_place1 = point_place(canvas_points[i], canvas_points[(i + 1) % 3])
+        point_place2 = point_place(canvas_points[i], canvas_points[(i + 2) % 3])
+
+        print('pp1', point_place1)
+        print('pp2', point_place2)
+
+        zone = 3
+        if point_place1 == point_place2:
+            zone = (point_place1 + 2) % 4
+
+
+        if zone == 0:
+            print('zone', 0)
+            x_place -= 50
+            y_place += 50
+        elif zone == 1:
+            print('zone', 1)
+            x_place += 50
+            y_place += 50
+        elif zone == 2:
+            print('zone', 2)
+            x_place += 50
+            y_place -= 50
+        else:
+            print('zone', 3)
+            x_place -= 50
+            y_place -= 50
+
+        print(x_place, y_place)
+
+        canvas.create_text(x_place, y_place, text=info, font=('Courier', 10))
