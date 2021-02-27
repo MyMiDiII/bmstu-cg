097fca33db3355dabf429005bea71f06902b4d0c
mmd19u540 <mymidi.student@yandex.ru>
2021-02-27 18:40:57 +0300

    ADD: scaling and printing point info

--- lab_01/graphic.py
+++ lab_01/graphic.py
@@ -12,11 +12,35 @@
 """
 
 
+def full_scale(canvas, points):
+    """
+        Преобразование координат для вывода
+        изображения в максимально возможном
+        масштабе
+    """
+
+    max_x = max(point[0] for point in points)
+    min_x = min(points, key=lambda x: x[0])[0]
+
+    max_y = max(points, key=lambda x: x[1])[1]
+    min_y = min(points, key=lambda x: x[1])[1]
+
+    print(canvas.winfo_reqwidth())
+    print(canvas.winfo_reqheight())
+    x_coef = (canvas.winfo_reqwidth() - canvas.winfo_reqwidth() / 5) / (max_x - min_x)
+    y_coef = (canvas.winfo_reqheight() - canvas.winfo_reqheight() / 5) / (max_y - min_y) 
+    print(x_coef, y_coef)
+
+    for point in points:
+        point[0] = (point[0] - min_x) * x_coef + canvas.winfo_reqwidth() / 10
+        point[1] = canvas.winfo_reqheight() - ((point[1] - min_y) * y_coef + canvas.winfo_reqheight() / 10)
+
+
 def create_point(canvas, point):
     """
         Функция отрисовки точки
     """
-    print(point)
+    # print(point)
 
     x1, y1 = (point[0] - 5), (point[1] - 5)
     x2, y2 = (point[0] + 5), (point[1] + 5)
@@ -30,3 +54,13 @@ def create_triangle(canvas, vertexes):
     """
 
     canvas.create_polygon(*vertexes, outline="#000000", fill="#ffffff", width=4)
+
+
+def print_point_info(canvas, nums, canvas_points, true_points):
+    """
+        Вывод номеров и координат точек
+    """
+
+    for i, point in enumerate(true_points):
+        info = "{:3d}({:6.2f}, {:6.2f})".format(nums[i] + 1, point[0], point[1])
+        canvas.create_text(canvas_points[i], text=info)
