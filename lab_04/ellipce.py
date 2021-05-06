66a3566778a08fc63f037606b9c7d40ec6433b1b
mmd19u540 <mymidi.student@yandex.ru>
2021-05-06 22:02:29 +0300

    ADD: canon for ellipse

--- lab_04/ellipce.py
+++ lab_04/ellipce.py
@@ -1,8 +1,53 @@
-def canon(Xc, Yc, Ra, Rb, scene):
+from math import sqrt, pi, cos, sin
+
+import point
+
+SQRT2 = sqrt(2)
+xSigns = [1, -1, -1, 1]
+ySigns = [1, 1, -1, -1]
+
+
+def canon(Xc, Yc, Ra, Rb, scene, pen):
     """
         Отрисовка окружности по
         каноническому уравнению
     """
+    doubleA = Ra * Ra
+    doubleB = Rb * Rb
+    xRange = doubleA / sqrt(doubleA + doubleB)
+    sqrtCoef = Rb / Ra
+    x = 0
+
+    while x <= xRange:
+        y = int(round(sqrtCoef * sqrt(doubleA - x * x)))
+
+        for i in range(4):
+            point.drawPoint(
+                Xc + xSigns[i] * x,
+                Yc + ySigns[i] * y,
+                scene,
+                pen
+            )
+
+        x += 1
+
+    y = 0
+    yRange = doubleB / sqrt(doubleA + doubleB)
+    sqrtCoef = Ra / Rb
+
+    while y <= yRange:
+        x = int(round(sqrtCoef * sqrt(doubleB - y * y)))
+
+        for i in range(4):
+            point.drawPoint(
+                Xc + xSigns[i] * x,
+                Yc + ySigns[i] * y,
+                scene,
+                pen
+            )
+
+        y += 1
+
     print("Ммм, канон!", Xc, Yc, Ra, Rb)
 
 
@@ -30,10 +75,10 @@ def midpoint(Xc, Yc, Ra, Rb, scene):
     print("Золотая середина", Xc, Yc, Ra, Rb)
 
 
-def libfunc(Xc, Yc, Ra, Rb, scene):
+def libfunc(Xc, Yc, Ra, Rb, scene, pen):
     """
         Отрисовка окружности
         библиотечной функции
     """
-    scene.addEllipse(Xc - Ra, Yc - Rb, 2 * Ra, 2 * Rb)
+    scene.addEllipse(Xc - Ra, Yc - Rb, 2 * Ra, 2 * Rb, pen)
     print("Все ради этого", Xc, Yc, Ra, Rb)
