b06aefbce0ca254b2af0a25259bbc563ebc1f04f
mmd19u540 <mymidi.student@yandex.ru>
2021-05-07 22:40:33 +0300

    ADD: brezenham and midpoint for ellipse

--- lab_04/ellipce.py
+++ lab_04/ellipce.py
@@ -12,14 +12,14 @@ def canon(Xc, Yc, Ra, Rb, scene, pen):
         Отрисовка эллипса по
         каноническому уравнению
     """
-    doubleA = Ra * Ra
-    doubleB = Rb * Rb
-    xRange = round(doubleA / sqrt(doubleA + doubleB))
+    sqrA = Ra * Ra
+    sqrB = Rb * Rb
+    xRange = round(sqrA / sqrt(sqrA + sqrB))
     sqrtCoef = Rb / Ra
     x = 0
 
     while x <= xRange:
-        y = int(round(sqrtCoef * sqrt(doubleA - x * x)))
+        y = int(round(sqrtCoef * sqrt(sqrA - x * x)))
 
         for i in range(4):
             point.drawPoint(
@@ -32,11 +32,11 @@ def canon(Xc, Yc, Ra, Rb, scene, pen):
         x += 1
 
     y = 0
-    yRange = round(doubleB / sqrt(doubleA + doubleB))
+    yRange = round(sqrB / sqrt(sqrA + sqrB))
     sqrtCoef = Ra / Rb
 
     while y <= yRange:
-        x = int(round(sqrtCoef * sqrt(doubleB - y * y)))
+        x = int(round(sqrtCoef * sqrt(sqrB - y * y)))
 
         for i in range(4):
             point.drawPoint(
@@ -77,19 +77,109 @@ def parametric(Xc, Yc, Ra, Rb, scene, pen):
     print("Все связано", Xc, Yc, Ra, Rb)
 
 
-def brezenham(Xc, Yc, Ra, Rb, scene):
+def brezenham(Xc, Yc, Ra, Rb, scene, pen):
     """
-        Отрисовка окружности
+        Отрисовка эллипса
         алгоритмом Брезенхема
     """
+    sqrA = Ra * Ra
+    sqrB = Rb * Rb
+
+    x = 0
+    y = Rb
+    capDelta = sqrB - sqrA * (2 * Rb - 1)
+
+    while y >= 0:
+        for i in range(4):
+            point.drawPoint(
+                Xc + xSigns[i] * x,
+                Yc + ySigns[i] * y,
+                scene,
+                pen
+            )
+
+        # TODO выделить функции
+        if capDelta < 0:
+            delta = 2 * capDelta + sqrA * (2 * y - 1)
+
+            if delta <= 0:
+                x += 1
+                capDelta = capDelta + sqrB * (2 * x + 1)
+            else:
+                x += 1
+                y -= 1
+                capDelta = capDelta + 2 * x * sqrB - 2 * y * sqrA + sqrA + sqrB
+
+        elif capDelta > 0:
+            delta = 2 * capDelta - sqrB * (2 * x + 1)
+
+            if delta <= 0:
+                x += 1
+                y -= 1
+                capDelta = capDelta + 2 * x * sqrB - 2 * y * sqrA + sqrA + sqrB
+            else:
+                y -= 1
+                capDelta = capDelta + sqrA * (- 2 * y + 1)
+
+        else:
+            x += 1
+            y -= 1
+            capDelta = capDelta + 2 * x * sqrB - 2 * y * sqrA + sqrA + sqrB
     print("От него не спрятаться", Xc, Yc, Ra, Rb)
 
 
-def midpoint(Xc, Yc, Ra, Rb, scene):
+def midpoint(Xc, Yc, Ra, Rb, scene, pen):
     """
-        Отрисовка окружности
+        Отрисовка эллипса
         алгоритмом средней точки
     """
+    sqrA = Ra * Ra
+    sqrB = Rb * Rb
+
+    x = 0
+    y = Rb
+    trialFunc = sqrB - sqrA * (Rb - 1 / 4)
+    xRange = int(round(sqrA / sqrt(sqrA + sqrB)))
+
+    while x <= xRange:
+        for i in range(4):
+            point.drawPoint(
+                Xc + xSigns[i] * x,
+                Yc + ySigns[i] * y,
+                scene,
+                pen
+            )
+
+        x += 1
+
+        if trialFunc > 0:
+            y -= 1
+            trialFunc -= 2 * y * sqrA
+
+        trialFunc = trialFunc + sqrB * (2 * x + 1)
+
+    x = Ra
+    y = 0
+    trialFunc = sqrA - sqrB * (Ra - 1 / 4)
+    yRange = int(round(sqrB / sqrt(sqrA + sqrB)))
+
+    while y <= yRange:
+        for i in range(4):
+            point.drawPoint(
+                Xc + xSigns[i] * x,
+                Yc + ySigns[i] * y,
+                scene,
+                pen
+            )
+
+        y += 1
+
+        if trialFunc > 0:
+            x -= 1
+            trialFunc -= 2 * x * sqrB
+
+        trialFunc = trialFunc + sqrA * (2 * y + 1)
+
     print("Золотая середина", Xc, Yc, Ra, Rb)
 
 
