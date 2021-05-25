775b6471f7b7ec9545793e7274d69364d6c81833
mmd19u540 <mymidi.student@yandex.ru>
2021-05-25 18:17:31 +0300

    FIX: window name

--- lab_05/MainWindow.py
+++ lab_05/MainWindow.py
@@ -144,7 +144,7 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "Maslova Lab05"))
         item = self.pointsTable.horizontalHeaderItem(0)
         item.setText(_translate("MainWindow", "X"))
         item = self.pointsTable.horizontalHeaderItem(1)
